0f0f87c5d0b6a6579bcc2b23b1fb6599f3986b6b
Kerby Eugene <1856753@cegepmontpetit.ca>
2025-03-06 01:24:22 -0500

    DEBUG
    Debug sur les concert

--- Tp2_kerby_Eugene/src/app/services/youtube.service.ts
+++ Tp2_kerby_Eugene/src/app/services/youtube.service.ts
@@ -7,11 +7,18 @@ import { lastValueFrom } from 'rxjs';
 })
 export class YoutubeService {
   songName:string=""
+  artistName:string=""
   cle:string="AIzaSyB6E2LuMeGqfoZFct0mVPxJZD4RCl-Wgfc"
+  cleConcert:string=""
   constructor(public http : HttpClient){}
   
    async getvideo(){
     let x= await lastValueFrom(this.http.get<any>(`https://www.googleapis.com/youtube/v3/search?type=video&part=id&maxResults=1&key=${this.cle}&q=${this.songName}`)) 
     return x.items[0].id.videoId
+  }
+
+  async getshows(){
+    let x= await lastValueFrom(this.http.get<any>(`https://rest.bandsintown.com/artists/${this.artistName}/events?app_id=${this.cleConcert}`))
+    console.log(x);
   }
 }
--- Tp2_kerby_Eugene/src/app/shows/shows.component.ts
+++ Tp2_kerby_Eugene/src/app/shows/shows.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { ActivatedRoute } from '@angular/router';
+import { ArtistsService } from '../services/artists.service';
+import { YoutubeService } from '../services/youtube.service';
 
 @Component({
   selector: 'app-shows',
   standalone: true,
-  imports: [],
+  imports: [GoogleMapsModule],
   templateUrl: './shows.component.html',
   styleUrl: './shows.component.css'
 })
 export class ShowsComponent {
-
+constructor(public route: ActivatedRoute, public artistService: ArtistsService,public youtubeService: YoutubeService,){}
+async ngOnInit() {
+await this.youtubeService.getshows()
+}
 }
